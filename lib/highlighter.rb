08ebb7e517dd2add48db0cb40ce4f93973ff479e
Rob <robert.hurring@lexisnexis.com>
2010-01-20 19:24:58 +0000

    highlighter helpers

---
+++ lib/highlighter.rb
@@ -0,0 +1,34 @@
+module Highlighter
+  
+  DefaultBrush    = 'shBrushPlain'
+  DefaultLanguage = 'plain'
+  BrushMap = {
+    'shBrushBash'       => ['sh', 'bash', 'shell'],
+    'shBrushCss'        => ['css'],
+    'shBrushDiff'       => ['diff', 'patch'],
+    'shBrushJava'       => ['java'],
+    'shBrushJScript'    => ['js', 'jscript', 'javascript'],
+    'shBrushObjC'       => ['objc', 'o', 'm'],
+    'shBrushPerl'       => ['pl', 'perl'],
+    'shBrushPhp'        => ['php'],
+    'shBrushPython'     => ['py', 'python'],
+    'shBrushRuby'       => ['rb', 'ruby', 'rails', 'ror'],
+    'shBrushSql'        => ['sql'],
+    'shBrushXml'        => ['xml', 'html', 'xhtml']
+  }
+  
+  def self.brush_for_language(language)
+    brush = BrushMap.select{ |brush, aliases| return brush if aliases.include?(language) }
+    brush = DefaultBrush if brush.empty?
+  end
+  
+  def self.language_for_extension(extension)
+    brush = BrushMap.select{ |brush, aliases| return extension if aliases.include?(extension) }
+    brush = DefaultLanguage if brush.empty?
+  end
+  
+  def self.brush_for_extension(extension)
+    brush_for_language(language_for_extension(extension))    
+  end
+  
+end
