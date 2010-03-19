7385d22a3e91a1081399179beecb07708b5f96d5
Rob Hurring <robert.hurring@lexisnexis.com>
2010-03-19 14:14:55 -0400

    no need for separate files. concating into the helpers file

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,7 +1,55 @@
-require 'helpers/url_helpers'
-require 'helpers/form_helpers'
-
 module Sinatra::Helpers
-  include URLHelpers
-  include FormHelpers
+# URL Helpers
+  def link_to(title, path, *args)
+    options = args.extract_options!
+    html_options = options.delete(:html) || {}
+    
+    "<a href='%s' %s>%s</a>" % [url_for(path, options), html_options.to_attributes, title]
+  end
+
+  def image_tag(path, *args)
+    options = args.extract_options!
+    path_options = options.delete(:path) || {}
+    path = '/images/' + path
+    
+    "<img src='%s' %s/>" % [url_for(path, path_options), options.to_attributes]
+  end
+  
+  def url_for(url_fragment, *args)
+    options = args.extract_options!
+    mode = options.delete(:mode) || :path_only
+
+    unless url_fragment =~ /^http/
+      case mode
+      when :path_only
+        base = request.script_name
+      when :full
+        scheme = request.scheme
+        if (scheme == 'http' && request.port == 80 ||
+            scheme == 'https' && request.port == 443)
+          port = ""
+        else
+          port = ":#{request.port}"
+        end
+        base = "#{scheme}://#{request.host}#{port}#{request.script_name}"
+      else
+        raise TypeError, "Unknown url_for mode #{mode}"
+      end
+      url = "#{base}#{url_fragment}"
+    else
+      url = url_fragment
+    end
+    
+    params = options.to_params
+    params = '?' + params unless params.empty?
+    "#{url}#{params}"
+  end
+  
+# Form Helpers
+  def error_for(object, attribute, title = nil)
+    if message = object.errors.on(attribute)
+      message = Array(message).inject(''){ |o, m| o + "#{title || attribute} #{m}<br/>" }
+      "<div class='form_error'>#{message}</div>"
+    end
+  end
 end
--- lib/helpers/form_helpers.rb
+++
@@ -1,9 +0,0 @@
-module FormHelpers
-  def error_for(object, attribute, title = nil)
-    if message = object.errors.on(attribute)
-      message = Array(message).inject(''){ |o, m| o + "#{title || attribute} #{m}<br/>" }
-      "<div class='form_error'>#{message}</div>"
-    end
-  end
-  
-end
