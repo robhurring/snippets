7e083d0fe99b0ae0e1f38879d3f386c33e61ac23
Rob Hurring <robert.hurring@lexisnexis.com>
2010-03-19 14:17:17 -0400

    removing engine opts since were switching to sqlite3

--- migrations/001_create_base_tables.rb
+++ migrations/001_create_base_tables.rb
@@ -1,13 +1,13 @@
 class CreateBaseTables < ActiveRecord::Migration
   def self.up
-		create_table :snippets, :force => true, :options => 'ENGINE=InnoDB DEFAULT CHARSET=utf8' do |t|
+		create_table :snippets, :force => true do |t|
 		  t.string :title
 		  t.integer :version, :default => 1
       t.text :data
       t.timestamps
     end
     
-		create_table :revisions, :force => true, :options => 'ENGINE=InnoDB DEFAULT CHARSET=utf8' do |t|
+		create_table :revisions, :force => true do |t|
       t.integer :snippet_id
       t.integer :version, :default => 1
 		  t.text :data
